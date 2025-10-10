2abd4149d95ef1ba3afa11836aa4276010695338
sandiniwijesinghe96 <47654167+sandiniwijesinghe96@users.noreply.github.com>
2025-10-10 11:54:36 +0530

    about me index.js

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,15 @@
+import "../AboutMe/styles.css";
+
+const AboutMe = () => {
+    return (
+        <div className="about-me-section">
+            <h1 className="about-me-title">About Me</h1>
+            <p className="about-me-text">
+                A passionate Software Engineer based in Sri Lanka with 3+ years of experience building scalable, user-friendly, and impactful software solutions. During this time, I’ve developed applications across diverse domains—from enterprise platforms at Sysco Labs to innovative projects like my Master’s research on a design pattern recommendation system with explainable AI.
+                With expertise in React, Next.js, Redux, Java, Spring Boot, Dropwizard, and cloud technologies such as AWS, I thrive on turning complex problems into elegant solutions. Beyond coding, I value teamwork, adaptability, and continuous learning, which continue to drive me to grow as both a professional and a person.
+            </p>
+        </div>
+    );
+};
+
+export default AboutMe;
